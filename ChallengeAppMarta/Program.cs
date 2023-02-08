4cc99c8b3507083ce33d3cb5e5c8863ccb93470d
Marta Stefanska <marta.stefanska@outlook.es>
2023-02-08 21:44:35 +0100

    Zadanie domowe dzien4

--- ChallengeAppMarta/Program.cs
+++ ChallengeAppMarta/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Marta";
+bool women = true;
+int age = 50;
+if (name == "Ewa" && age < 50)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (!women == true && age != 50)
+{
+    Console.WriteLine("Mezczyzna Twojego Zycia");
+}
+else if (women == true && age < 50)
+{
+    Console.WriteLine("Kobieta ponizej 50 lat");
+}
+else
+{
+    Console.WriteLine("Marta, lat 52 poczatkujacy programista C#");
+}
+
