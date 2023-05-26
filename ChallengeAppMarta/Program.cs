e032449af54c1c08ffe5bc097a93d978ad7169b3
Marta Stefanska <marta.stefanska@outlook.es>
2023-05-26 21:14:21 +0200

    Zadanie domowe dzien 5

--- ChallengeAppMarta/Program.cs
+++ ChallengeAppMarta/Program.cs
@@ -1,20 +1,14 @@
-﻿var name = "Marta";
-bool women = true;
-int age = 50;
-if (name == "Ewa" && age < 50)
-{
-    Console.WriteLine("Ewa, lat 33");
-}
-else if (!women && age != 50)
-{
-    Console.WriteLine("Mezczyzna Twojego Zycia");
-}
-else if (women == true && age < 50)
-{
-    Console.WriteLine("Kobieta ponizej 50 lat");
-}
-else
-{
-    Console.WriteLine("Marta, lat 52 poczatkujacy programista C#");
-}
+﻿int number = 45661133;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToCharArray();
 
+for (int i = 0; i < 10; i++)
+{
+    int counter = 0;
+    foreach (char digit in letters)
+    {
+        if (int.Parse(digit.ToString()) == i)
+        { counter++; }
+    }
+    Console.WriteLine(i + "=>" + counter);
+}
