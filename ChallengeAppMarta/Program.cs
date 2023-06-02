e6307bb8dbf6a213f616d51bfe65ae7ebbe46ac4
Marta Stefanska <marta.stefanska@outlook.es>
2023-06-02 18:08:25 +0200

    Zadanie domowe Dzien 6

--- ChallengeAppMarta/Program.cs
+++ ChallengeAppMarta/Program.cs
@@ -1,14 +1,45 @@
-﻿int number = 45661133;
-string numberAsString = number.ToString();
-char[] letters = numberAsString.ToCharArray();
+﻿using ChallengeAppMarta;
 
-for (int i = 0; i < 10; i++)
+Employee employee1 = new Employee ("Gosia","Pocahontas", 45);
+Employee employee2 = new Employee ("Iwona", "Kopciuszek", 60);
+Employee employee3 = new Employee ("Magda", "Spiaca", 24);
+Employee employee4 = new Employee ("Wicky", "Sniezka", 23);
+
+employee1.AddScore(8);
+employee1.AddScore(-2);
+employee1.AddScore(9);
+employee1.AddScore(-1);
+
+employee2.AddScore(8);
+employee2.AddScore(0);
+employee2.AddScore(10);
+employee2.AddScore(-1);
+
+employee3.AddScore(6);
+employee3.AddScore(-1);
+employee3.AddScore(7);
+employee3.AddScore(0);
+
+employee4.AddScore(7);
+employee4.AddScore(-1);
+employee4.AddScore(8);
+employee4.AddScore(-1);
+
+List<Employee> employees = new List<Employee>()
 {
-    int counter = 0;
-    foreach (char digit in letters)
-    {
-        if (int.Parse(digit.ToString()) == i)
-        { counter++; }
-    }
-    Console.WriteLine(i + "=>" + counter);
+    employee1,employee2,employee3,employee4
+};
+
+int maxResult = 0;
+Employee bestEmployee = null;
+
+foreach (var employee in employees) 
+{
+    if (employee.Result> maxResult)
+    { bestEmployee = employee; }
 }
+
+Console.WriteLine(
+"Najlepszy pracownik: " + bestEmployee.Name +" "+ bestEmployee.Surname+", lat "+
+bestEmployee.Age+", uzyskala wynik "+bestEmployee.Result + " punktow");
+ 
