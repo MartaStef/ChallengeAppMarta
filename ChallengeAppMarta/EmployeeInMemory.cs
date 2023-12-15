dc1ec13b56788319820af03f1590b22eadb816db
Marta Stefanska <marta.stefanska@outlook.es>
2023-12-15 13:18:30 +0100

    Zadanie domowe dzien 16

--- ChallengeAppMarta/EmployeeInMemory.cs
+++ ChallengeAppMarta/EmployeeInMemory.cs
@@ -1,67 +1,59 @@
-﻿
-using System;
-
-namespace ChallengeAppMarta
+﻿namespace ChallengeAppMarta
 {
-    public class Employee: IEmployee
+    internal class EmployeeInMemory : EmployeeBase
     {
         private List<float> grades = new List<float>();
-        
-        public Employee(string name, string surname, int age)
-        {    
-            this.Name = name;
-            this.Surname = surname;
-            this.Age = age;
-        }
-        public string Name { get; private set; }
-        public string Surname { get; private set; }
-        public int Age { get; private set; }
-       
-        public void AddGrade(float grade)
+        public EmployeeInMemory(string name, string surname)
+            : base(name, surname)
         {
-            if(grade >= 0 && grade <= 100)
+        }
+
+        public override void AddGrade(float grade)
+        {
+            if (grade >= 0 && grade <= 100)
             {
                 this.grades.Add(grade);
             }
             else
             {
-                throw new Exception ("Invalid grade value");
+                throw new Exception("Invalid grade value");
             }
         }
 
-        public void AddGrade(string grade)
+        public override void AddGrade(string grade)
         {
-            if(float.TryParse(grade, out float result))
+            if (float.TryParse(grade, out float result))
             {
                 //var value = float.Parse(grade);
                 //this.AddGrade(value);
                 this.AddGrade(result);
             }
-            else if (grade.Length==1)
+            else if (grade.Length == 1)
             {
                 char.TryParse(grade, out char CharResult);
                 this.AddGrade(CharResult);
             }
             else
             {
-                throw new Exception ("String is not float");
+                throw new Exception("String is not float");
             }
         }
 
-        public void AddGrade(long grade) 
+        public override void AddGrade(long grade)
         {
             float result = (float)grade;
             this.AddGrade(result);
         }
 
-        public void AddGrade(double grade)
-        { 
-           float result = (float)Math.Round(grade, 2);
+        public override void AddGrade(double grade)
+        {
+            float result = (float)Math.Round(grade, 2);
             this.AddGrade(result);
         }
-        public void AddGrade(char grade)
+
+        public override void AddGrade(char grade)
         {
-            switch(grade)
+            switch (grade)
             {
                 case 'A':
                 case 'a':
@@ -84,24 +76,25 @@ namespace ChallengeAppMarta
                     this.AddGrade(20);
                     break;
                 default:
-                    throw new Exception ("Wrong letter");                   
+                    throw new Exception("Wrong letter");
             }
         }
-        public Statistics GetStatistics() 
+
+        public override Statistics GetStatistics()
         {
             var statistics = new Statistics();
             statistics.Average = 0;
             statistics.Max = float.MinValue;
             statistics.Min = float.MaxValue;
 
-            foreach (var grade in this.grades) 
+            foreach (var grade in this.grades)
             {
                 statistics.Max = Math.Max(statistics.Max, grade);
                 statistics.Min = Math.Min(statistics.Min, grade);
                 statistics.Average += grade;
             }
             statistics.Average /= this.grades.Count;
-            switch(statistics.Average)
+            switch (statistics.Average)
             {
                 case var average when average >= 80:
                     statistics.AverageLetter = 'A';
@@ -115,12 +108,11 @@ namespace ChallengeAppMarta
                 case var average when average >= 20:
                     statistics.AverageLetter = 'D';
                     break;
-                default: 
+                default:
                     statistics.AverageLetter = 'E';
                     break;
             }
             return statistics;
         }
-        
     }
 }
--- ChallengeAppMarta/Program.cs
+++ ChallengeAppMarta/Program.cs
@@ -3,28 +3,21 @@
 Console.WriteLine("Witamy w Programie do oceny pracowników");
 Console.WriteLine("=======================================");
 Console.WriteLine("Należy podać kolejno oceny pracownika: 0-100 lub litery a-e");
-Console.WriteLine("dla kierownika można dodatkowo w skali: 1-6 (z '+' i '-')");
 Console.WriteLine("Aby wyświetlić statystyki należy wpisać literę q");
 Console.WriteLine();
 
-var employee = new Employee("Marta", "Stefanska", 53);
-var supervisor = new Supervisor("Marta","Stefanska",'K', 53);
+var employee = new EmployeeInFile("Marta", "Stefanska");
 
-bool exit = true;
 
-while (exit)
-{
-    Console.WriteLine("Wybierz pracownika: p lub kierownika: k");
-    var option = Console.ReadLine();
-    if (option == "p")
-    {
+
+
+
         while (true)
         {
             Console.WriteLine("Podaj ocenę pracownika: ");
             var input = Console.ReadLine();
             if (input == "q" || input == "Q")
             {
-                exit = false;
                 break;
             }
 
@@ -37,38 +30,10 @@ while (exit)
                 Console.WriteLine($"Exception catched: {e.Message}");
             }
         }
-        var statistics1 = employee.GetStatistics();
-        Console.WriteLine($"Average: {statistics1.Average}");
-        Console.WriteLine($"Max: {statistics1.Max}");
-        Console.WriteLine($"Min: {statistics1.Min}");
-    }
+        var statistics = employee.GetStatistics();
+        Console.WriteLine($"Average: {statistics.Average}");
+        Console.WriteLine($"Max: {statistics.Max}");
+        Console.WriteLine($"Min: {statistics.Min}");
+    
 
-    if (option == "k")
-    {
-        while (true)
-        {
-            Console.WriteLine("Podaj ocenę kierownika: ");
-            var input = Console.ReadLine();
-            if (input == "q" || input == "Q")
-            {
-                exit = false;
-                break;
-            }
-
-            try
-            {
-                supervisor.AddGrade(input);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Exception catched: {e.Message}");
-            }
-        }
-        var statistics2 = supervisor.GetStatistics();
-        Console.WriteLine($"Average: {statistics2.Average}");
-        Console.WriteLine($"Max: {statistics2.Max}");
-        Console.WriteLine($"Min: {statistics2.Min}");
-    }
-}
-
-
+   
